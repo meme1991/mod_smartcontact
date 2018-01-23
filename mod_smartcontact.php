6eaad7074bed4f08c9c168873ea91cbec5441e71
Emanuele Cappato <e.kappato@icloud.com>
2018-01-23 11:42:20 +0100

    fix

--- mod_smartcontact.php
+++ mod_smartcontact.php
@@ -1,11 +1,11 @@
 <?php
-/**
- * @version    2.0.x
- * @package    Smart Contact
- * @author     SPEDI srl http://www.spedi.it
- * @copyright  Copyright (c) 1991 - 2016 Spedi srl. Tutti i diritti riservati.
- * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
- */
+# @Author: SPEDI srl
+# @Date:   23-01-2018
+# @Email:  sviluppo@spedi.it
+# @Last modified by:   SPEDI srl
+# @Last modified time: 23-01-2018
+# @License: GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+# @Copyright: Copyright (C) SPEDI srl
 
 defined('_JEXEC') or die;
 
